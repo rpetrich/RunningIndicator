ac83b4daf91e9d9c86e16a6d9bffb5fd757a6cd2
Daniel Ferreira (theiostream) <daniel.calibeta@gmail.com>
2013-01-24 12:25:24 -0200

    Make the tweak compilable for people with no SpringBoard headers and do not show the X for root-owned apps.

--- SpringBoard.h
+++ SpringBoard.h
@@ -1,211 +1,43 @@
-#import <SpringBoard/SpringBoard.h>
-
-@interface SBIconModel (iOS40)
-- (SBApplicationIcon *)applicationIconForDisplayIdentifier:(NSString *)displayIdentifier;
+@interface SBApplication : NSObject
+- (void)kill;
+- (NSBundle *)bundle;
+- (NSString *)displayIdentifier;
 @end
 
-@interface SBIcon (iOS40)
-- (void)prepareDropGlow;
-- (UIImageView *)dropGlow;
-- (void)showDropGlow:(BOOL)showDropGlow;
-- (void)setShowsCloseBox:(BOOL)showsCloseBox;
+@interface SBIcon : NSObject
 @end
 
-@interface SBIconController (iOS40)
+@interface SBIcon (iOS4)
+- (void)setShowsImages:(BOOL)shows;
+- (void)setShowsCloseBox:(BOOL)show;
+@end
+
+@interface SBIconController : NSObject
++ (id)sharedInstance;
+- (BOOL)isEditing;
 - (BOOL)canUninstallIcon:(SBIcon *)icon;
 @end
 
-@protocol SBIconViewDelegate, SBIconViewLocker;
-@class SBIconImageContainerView, SBIconBadgeImage;
-
-@interface SBIconView : UIView {
-	SBIcon *_icon;
-	id<SBIconViewDelegate> _delegate;
-	id<SBIconViewLocker> _locker;
-	SBIconImageContainerView *_iconImageContainer;
-	SBIconImageView *_iconImageView;
-	UIImageView *_iconDarkeningOverlay;
-	UIImageView *_ghostlyImageView;
-	UIImageView *_reflection;
-	UIImageView *_shadow;
-	SBIconBadgeImage *_badgeImage;
-	UIImageView *_badgeView;
-	SBIconLabel *_label;
-	BOOL _labelHidden;
-	BOOL _labelOnWallpaper;
-	UIView *_closeBox;
-	int _closeBoxType;
-	UIImageView *_dropGlow;
-	unsigned _drawsLabel : 1;
-	unsigned _isHidden : 1;
-	unsigned _isGrabbed : 1;
-	unsigned _isOverlapping : 1;
-	unsigned _refusesRecipientStatus : 1;
-	unsigned _highlighted : 1;
-	unsigned _launchDisabled : 1;
-	unsigned _isJittering : 1;
-	unsigned _allowJitter : 1;
-	unsigned _touchDownInIcon : 1;
-	unsigned _hideShadow : 1;
-	NSTimer *_delayedUnhighlightTimer;
-	unsigned _onWallpaper : 1;
-	unsigned _ghostlyRequesters;
-	int _iconLocation;
-	float _iconImageAlpha;
-	float _iconImageBrightness;
-	float _iconLabelAlpha;
-	float _accessoryAlpha;
-	CGPoint _unjitterPoint;
-	CGPoint _grabPoint;
-	NSTimer *_longPressTimer;
-	unsigned _ghostlyTag;
-	UIImage *_ghostlyImage;
-	BOOL _ghostlyPending;
-}
-+ (CGSize)defaultIconSize;
-+ (CGSize)defaultIconImageSize;
-+ (BOOL)allowsRecycling;
-+ (id)_jitterPositionAnimation;
-+ (id)_jitterTransformAnimation;
-
-- (id)initWithDefaultSize;
-- (void)dealloc;
-
-@property(assign) id<SBIconViewDelegate> delegate;
-@property(assign) id<SBIconViewLocker> locker;
-@property(readonly, retain) SBIcon *icon;
-- (void)setIcon:(SBIcon *)icon;
-
-- (int)location;
-- (void)setLocation:(int)location;
-- (void)showIconAnimationDidStop:(id)showIconAnimation didFinish:(id)finish icon:(id)icon;
-- (void)setIsHidden:(BOOL)hidden animate:(BOOL)animate;
-- (BOOL)isHidden;
-- (BOOL)isRevealable;
-- (void)positionIconImageView;
-- (void)applyIconImageTransform:(CATransform3D)transform duration:(float)duration delay:(float)delay;
-- (void)setDisplayedIconImage:(id)image;
-- (id)snapshotSettings;
-- (id)iconImageSnapshot:(id)snapshot;
-- (id)reflectedIconWithBrightness:(CGFloat)brightness;
-- (void)setIconImageAlpha:(CGFloat)alpha;
-- (void)setIconLabelAlpha:(CGFloat)alpha;
-- (SBIconImageView *)iconImageView;
-- (void)setLabelHidden:(BOOL)hidden;
-- (void)positionLabel;
-- (CGSize)_labelSize;
-- (Class)_labelClass;
-- (void)updateLabel;
-- (void)_updateBadgePosition;
-- (id)_overriddenBadgeTextForText:(id)text;
-- (void)updateBadge;
-- (id)_automationID;
-- (BOOL)pointMostlyInside:(CGPoint)inside withEvent:(UIEvent *)event;
-- (CGRect)frameForIconOverlay;
-- (void)placeIconOverlayView;
-- (void)updateIconOverlayView;
-- (void)_updateIconBrightness;
-- (BOOL)allowsTapWhileEditing;
-- (BOOL)delaysUnhighlightWhenTapped;
-- (BOOL)isHighlighted;
-- (void)setHighlighted:(BOOL)highlighted;
-- (void)setHighlighted:(BOOL)highlighted delayUnhighlight:(BOOL)unhighlight;
-- (void)_delayedUnhighlight;
-- (BOOL)isInDock;
-- (id)_shadowImage;
-- (void)_updateShadow;
-- (void)updateReflection;
-- (void)setDisplaysOnWallpaper:(BOOL)wallpaper;
-- (void)setLabelDisplaysOnWallpaper:(BOOL)wallpaper;
-- (BOOL)showsReflection;
-- (float)_reflectionImageOffset;
-- (void)setFrame:(CGRect)frame;
-- (void)setIsJittering:(BOOL)isJittering;
-- (void)setAllowJitter:(BOOL)allowJitter;
-- (BOOL)allowJitter;
-- (void)removeAllIconAnimations;
-- (void)setIconPosition:(CGPoint)position;
-- (void)setRefusesRecipientStatus:(BOOL)status;
-- (BOOL)canReceiveGrabbedIcon:(id)icon;
-- (double)grabDurationForEvent:(id)event;
-- (void)setIsGrabbed:(BOOL)grabbed;
-- (BOOL)isGrabbed;
-- (void)setIsOverlapping:(BOOL)overlapping;
-- (CGAffineTransform)transformToMakeDropGlowShrinkToIconSize;
-- (void)prepareDropGlow;
-- (void)showDropGlow:(BOOL)glow;
-- (void)removeDropGlow;
-- (id)dropGlow;
-- (BOOL)isShowingDropGlow;
-- (void)placeGhostlyImageView;
-- (id)_genGhostlyImage:(id)image;
-- (void)prepareGhostlyImageIfNeeded;
-- (void)prepareGhostlyImage;
-- (void)prepareGhostlyImageView;
-- (void)setGhostly:(BOOL)ghostly requester:(int)requester;
-- (void)setPartialGhostly:(float)ghostly requester:(int)requester;
-- (void)removeGhostlyImageView;
-- (BOOL)isGhostly;
-- (int)ghostlyRequesters;
-- (void)longPressTimerFired;
-- (void)cancelLongPressTimer;
-- (void)touchesCancelled:(id)cancelled withEvent:(id)event;
-- (void)touchesBegan:(id)began withEvent:(id)event;
-- (void)touchesMoved:(id)moved withEvent:(id)event;
-- (void)touchesEnded:(id)ended withEvent:(id)event;
-- (BOOL)isTouchDownInIcon;
-- (void)setTouchDownInIcon:(BOOL)icon;
-- (void)hideCloseBoxAnimationDidStop:(id)hideCloseBoxAnimation didFinish:(id)finish closeBox:(id)box;
-- (void)positionCloseBoxOfType:(int)type;
-- (id)_newCloseBoxOfType:(int)type;
-- (void)setShowsCloseBox:(BOOL)box;
-- (void)setShowsCloseBox:(BOOL)box animated:(BOOL)animated;
-- (BOOL)isShowingCloseBox;
+@interface SBApplicationIcon : SBIcon
+- (SBApplication *)application;
 - (void)closeBoxTapped;
-- (BOOL)pointInside:(CGPoint)inside withEvent:(id)event;
-- (UIEdgeInsets)snapshotEdgeInsets;
-- (void)setShadowsHidden:(BOOL)hidden;
-- (void)_updateShadowFrameForShadow:(id)shadow;
-- (void)_updateShadowFrame;
-- (BOOL)_delegatePositionIsEditable;
-- (void)_delegateTouchEnded:(BOOL)ended;
-- (BOOL)_delegateTapAllowed;
-- (int)_delegateCloseBoxType;
-- (id)createShadowImageView;
-- (void)prepareForRecycling;
-- (CGRect)defaultFrameForProgressBar;
-- (void)iconImageDidUpdate:(id)iconImage;
-- (void)iconAccessoriesDidUpdate:(id)iconAccessories;
-- (void)iconLaunchEnabledDidChange:(id)iconLaunchEnabled;
 @end
 
-@class NSMapTable;
+@interface SBIconView : UIView
+- (void)setShowsImages:(BOOL)shows;
+- (void)prepareDropGlow;
+- (UIImageView *)dropGlow;
+- (void)showDropGlow:(BOOL)show;
+- (void)setShowsCloseBox:(BOOL)show;
+- (SBIcon *)icon;
+@end
 
-@interface SBIconViewMap : NSObject {
-	NSMapTable* _iconViewsForIcons;
-	id<SBIconViewDelegate> _iconViewdelegate;
-	NSMapTable* _recycledIconViewsByType;
-	NSMapTable* _labels;
-	NSMapTable* _badges;
-}
-+(SBIconViewMap *)homescreenMap;
-+(Class)iconViewClassForIcon:(SBIcon *)icon location:(int)location;
--(id)init;
--(void)dealloc;
--(SBIconView *)mappedIconViewForIcon:(SBIcon *)icon;
--(SBIconView *)_iconViewForIcon:(SBIcon *)icon;
--(SBIconView *)iconViewForIcon:(SBIcon *)icon;
--(void)_addIconView:(SBIconView *)iconView forIcon:(SBIcon *)icon;
--(void)purgeIconFromMap:(SBIcon *)icon;
--(void)_recycleIconView:(SBIconView *)iconView;
--(void)recycleViewForIcon:(SBIcon *)icon;
--(void)recycleAndPurgeAll;
--(id)releaseIconLabelForIcon:(SBIcon *)icon;
--(void)captureIconLabel:(id)label forIcon:(SBIcon *)icon;
--(void)purgeRecycledIconViewsForClass:(Class)aClass;
--(void)_modelListAddedIcon:(SBIcon *)icon;
--(void)_modelRemovedIcon:(SBIcon *)icon;
--(void)_modelReloadedIcons;
--(void)_modelReloadedState;
--(void)iconAccessoriesDidUpdate:(SBIcon *)icon;
+@interface SBIconViewMap : NSObject
++ (id)homescreenMap;
+- (SBIconView *)mappedIconViewForIcon:(SBIcon *)icon;
+@end
+
+@interface SBIconModel : NSObject
++ (id)sharedInstance;
+- (SBApplicationIcon *)applicationIconForDisplayIdentifier:(NSString *)displayID;
 @end
